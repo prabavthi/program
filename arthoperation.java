06fa97f888ab3334f9c26a38e480cdb761dbb9df
prabavathi <prabavathia05@gmail.com>
2024-09-15 16:23:44 +0530

    Add files via upload

---
+++ arthoperation.java
@@ -0,0 +1,31 @@
+package myfiles.index;
+
+import java.util.Scanner;
+
+public class arthoperation {
+	
+    
+	
+	    public static void main(String args[])
+	  {
+	    int first, second, add, subtract, multiply;
+	    float devide;
+	    Scanner scanner = new Scanner(System.in);
+
+	    System.out.println("Enter Two Numbers : ");
+	    first = scanner.nextInt();
+	    second = scanner.nextInt();
+
+	    add = first + second;
+	    subtract = first - second;
+	    multiply = first * second;
+	    devide = (float) first / second;
+
+	    System.out.println("Sum = " + add);
+	    System.out.println("Difference = " + subtract);
+	    System.out.println("Multiplication = " + multiply);
+	    System.out.println("Division = " + devide);
+	  }
+	}
+
+
