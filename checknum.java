58a0ccf965af93985d546e88c4937ea696c570cf
prabavathi <prabavathia05@gmail.com>
2024-09-18 20:03:14 +0530

    Add files via upload

---
+++ checknum.java
@@ -0,0 +1,21 @@
+package myfiles.index;
+
+public class checknum {
+
+	public static void main(String[] args) {
+		int number=0;
+		if(number >0) {
+			System.out.println("this is possitive integer");
+		}
+		else if(number<0)
+		{
+			System.out.println("this is negative integer");
+		}
+		else
+		{
+			System.out.println("this is zero");
+		}
+		
+	}
+
+}
---
+++ empage.java
@@ -0,0 +1,26 @@
+package myfiles.index;
+
+public class empage {
+
+	public static void main(String[] args) {
+		String s="praba";
+		int id=47;
+		double db=50000;
+		
+		System.out.println("the employee name is " +s);
+		System.out.println("the employee id is " +id);
+		System.out.println("the employee salary is " +db);
+		
+		int num=90;
+		if(num<90)
+		{
+			System.out.println("you are not eligible to work in a company");
+		}
+		else
+		{
+			System.out.println("you are eligible to work in a company");
+		}
+
+	}
+
+}
