9a421ea662e9f089e8029c6e702dc90a7b14777e
prabavathi <prabavathia05@gmail.com>
2024-09-23 20:00:15 +0530

    Add files via upload

---
+++ publicmethod.java
@@ -0,0 +1,20 @@
+package myfiles.index;
+
+public class publicmethod {
+	public void number() {
+		int a=5;
+		int b=a+7;
+		System.out.println("the number is "+b);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		publicmethod test=new publicmethod();
+		test.number();
+
+	}
+
+}
+
+
+
---
+++ staticmethod.java
@@ -0,0 +1,14 @@
+package myfiles.index;
+
+public class staticmethod {
+
+
+	public static void main(String[] args) {
+		int a=5;
+		int b=9;
+		int sum=a+b;
+		System.out.println(sum);
+		
+	}
+
+}
